113dada707de17c799895232208cb04582e6fee6
Talha Arshad <talha.arshad@systemsltd.com>
2022-08-11 16:44:55 +0500

    redux toolkit tut completed

--- src/features/icecream/IcecreamView.jsx
+++ src/features/icecream/IcecreamView.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { ordered, restocked } from './icecreamSlice'
 
 export const IcecreamView = () => {
+  const [value, setValue] = useState(1)
+  const numOfIcecreams = useSelector((state) => state.icecream.numOfIcecreams)
+  const dispatch = useDispatch()
+
   return (
     <div>
-        <h2>Number of ice creams - </h2>
-        <button>Order ice cream</button>
-        <button>Restock ice creams</button>
+        <h2>Number of ice creams - {numOfIcecreams}</h2>
+        <button onClick={() => dispatch(ordered())}>Order ice cream</button>
+        <input type='number' value={value} onChange={e => setValue(parseInt(e.target.value))}/>
+        <button onClick={() => dispatch(restocked(value))}>Restock ice creams</button>
     </div>
   )
 }
